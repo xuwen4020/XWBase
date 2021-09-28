f05741d28866fb4616bc861e95b385628bc71640
xuwen <996592197@qq.com>
2021-09-28 14:10:16 +0800

    asdf

---
+++ Product/XWBase.framework/Headers/XWBase.h
@@ -0,0 +1,19 @@
+//
+//  XWBase.h
+//  XWBase
+//
+//  Created by XuWen on 2021/8/3.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for XWBase.
+FOUNDATION_EXPORT double XWBaseVersionNumber;
+
+//! Project version string for XWBase.
+FOUNDATION_EXPORT const unsigned char XWBaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XWBase/PublicHeader.h>
+
+#import <XWBase/Demo.h>
+
